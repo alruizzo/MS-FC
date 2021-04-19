e3eb1276385927372f3bee032b868f876342d587
alruizzo <adrizzo@gmail.com>
2021-04-19 09:25:00 +0200

    Update z_average_extraction_all_global.sh
    Use thresholded Melodic mask instead of all voxels above 0

--- z_average_extraction_all_global.sh
+++ z_average_extraction_all_global.sh
@@ -9,6 +9,7 @@
 # Specify where DR2 results are (subjects 4D with all components)
 DRFOLDER=/Users/lmuresearchfellowship/Documents/Adriana/LMU_Psychology/Projects/MS/DRstage2
 OUTPUTFOLDER=/Users/lmuresearchfellowship/Documents/Adriana/LMU_Psychology/Projects/MS/Randomise/globalFC
+MELODICFOLDER=/Volumes/adrizzo/CELSIUS/MS/ICA30/Individual_ICs
 
 echo "creating the output folder if it doesn't exist..."
 mkdir -p $OUTPUTFOLDER
@@ -20,13 +21,14 @@ do
   SUBJECT=$(basename "${i}")
   echo $SUBJECT >> $OUTPUTFOLDER/00_global_FC_participants.txt
   cd $i
-    fslstats $i/${SUBJECT}_0004.nii.gz -l 0 -M >> $OUTPUTFOLDER/01_global_FC_IC5_RFPN.txt
-    fslstats $i/${SUBJECT}_0005.nii.gz -l 0 -M >> $OUTPUTFOLDER/02_global_FC_IC6_PDMN.txt
-    fslstats $i/${SUBJECT}_0006.nii.gz -l 0 -M >> $OUTPUTFOLDER/03_global_FC_IC7_SMN2.txt
-    fslstats $i/${SUBJECT}_0007.nii.gz -l 0 -M >> $OUTPUTFOLDER/04_global_FC_IC8_LFPN.txt
-    fslstats $i/${SUBJECT}_0010.nii.gz -l 0 -M >> $OUTPUTFOLDER/05_global_FC_IC11_ADMN.txt
-    fslstats $i/${SUBJECT}_0011.nii.gz -l 0 -M >> $OUTPUTFOLDER/06_global_FC_IC12_SMN1.txt
-    fslstats $i/${SUBJECT}_0023.nii.gz -l 0 -M >> $OUTPUTFOLDER/07_global_FC_IC24_BG.txt
+    # Obtain the mean within the thresholded MELODIC component (vs. all values above 0: "-l 0")
+    fslstats $i/${SUBJECT}_0004.nii.gz -k $MELODICFOLDER/thr_vol0004.nii.gz -M >> $OUTPUTFOLDER/01_global_FC_IC5_RFPN.txt
+    fslstats $i/${SUBJECT}_0005.nii.gz -k $MELODICFOLDER/thr_vol0005.nii.gz -M >> $OUTPUTFOLDER/02_global_FC_IC6_PDMN.txt
+    fslstats $i/${SUBJECT}_0006.nii.gz -k $MELODICFOLDER/thr_vol0006.nii.gz -M >> $OUTPUTFOLDER/03_global_FC_IC7_SMN2.txt
+    fslstats $i/${SUBJECT}_0007.nii.gz -k $MELODICFOLDER/thr_vol0007.nii.gz -M >> $OUTPUTFOLDER/04_global_FC_IC8_LFPN.txt
+    fslstats $i/${SUBJECT}_0010.nii.gz -k $MELODICFOLDER/thr_vol0010.nii.gz -M >> $OUTPUTFOLDER/05_global_FC_IC11_ADMN.txt
+    fslstats $i/${SUBJECT}_0011.nii.gz -k $MELODICFOLDER/thr_vol0011.nii.gz -M >> $OUTPUTFOLDER/06_global_FC_IC12_SMN1.txt
+    fslstats $i/${SUBJECT}_0023.nii.gz -k $MELODICFOLDER/thr_vol0023.nii.gz -M >> $OUTPUTFOLDER/07_global_FC_IC24_BG.txt
 done
 
 # If I wanted to use names for files as column headers,...
